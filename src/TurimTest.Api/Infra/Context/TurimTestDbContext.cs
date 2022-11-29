fb7e5b5a593004c4c56dcf6da0ed995b38d9f85b
roberio <roberio.pb@hotmail.com>
2022-11-28 21:42:58 -0300

    Adicionar arquivos de projeto.

---
+++ src/TurimTest.Api/Infra/Context/TurimTestDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TurimTest.Api.Infra.Context;
+
+public class TurimTestDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase(databaseName: "TurimTest");
+    }
+}
